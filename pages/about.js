4e6e8730076fbd35ee7958f98f55ebf8ad581394
heremyas <jhem2911@gmail.com>
2022-03-15 15:30:22 +0800

    removed spaces

--- pages/about.js
+++ pages/about.js
@@ -8,9 +8,27 @@ function about() {
     <div className={s.About}>
       <Navbar currentPage={2} />
 
-      <div className={s.txt_container}> 
-        <h4>I am a <span style={{fontSize: '1.7em'}}>Computer Engineering Student</span> <span>who fell inlove with <span style={{color: '#00C2CB', fontSize: '1.4em'}}>Software</span>. I like discovering new technologies and building projects from scratch.</span> <span style={{color: '#00C2CB'}}>When I don't have assignments, I usually code and when I have, I still code. </span><br></br><br></br>Apart from coding, I shred my guitar during breaktime.
-       </h4> 
+      <div className={s.txt_container}>
+        <h4>
+          I am a{" "}
+          <span style={{ fontSize: "1.7em" }}>
+            Computer Engineering Student
+          </span>{" "}
+          <span>
+            who fell inlove with{" "}
+            <span style={{ color: "#00C2CB", fontSize: "1.4em" }}>
+              Software
+            </span>
+            . I like discovering new technologies and building projects from
+            scratch.
+          </span>
+          <span style={{ color: "#00C2CB" }}>
+            When I don't have assignments, I usually code and when I have, I
+            still code.
+          </span>
+          <br></br>
+          <br></br>Apart from coding, I shred my guitar during breaktime.
+        </h4>
       </div>
 
       <MyPic />
