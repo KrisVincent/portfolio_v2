e88fe264c022219be37d3167d63943306e9e6021
heremyas <62454436+heremyas@users.noreply.github.com>
2023-08-10 03:59:44 +0800

    [FEATURE] Added typewriter effect in popup big texts in Home and About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,7 @@ import MyPic from "../components/MyPic";
 import Navbar from "../components/Navbar";
 import s from "../styles/About.module.css";
 import { NAVBAR_ITEMS } from "../enums";
+import TypewriterEffect from "../components/typewriterEffect";
 
 function about() {
   return (
@@ -14,28 +15,21 @@ function about() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar active={NAVBAR_ITEMS.About} />
-
       <div className={s.txt_container}>
-        <h4>
-          I am a<span style={{ fontSize: "1.7em" }}> Computer Engineer</span>
-          <span>
-            {" "}
-            who fell inlove with
-            <span style={{ color: "#00C2CB", fontSize: "1.4em" }}>
-              {" "}
-              Software
-            </span>
-            . I like discovering new technologies and building projects from
-            scratch.{" "}
-          </span>
-          <span style={{ color: "#00C2CB" }}>
-            {`When it's my free time, I code, when it's not, I still code.`}
-          </span>
+        <TypewriterEffect>
+          <h5>I am a Computer Engineer</h5>
+          <h3>
+            who fell inlove with Software I like discovering new technologies
+            and building projects from scratch.
+          </h3>
+          <h3 style={{ color: "#00C2CB" }}>
+            When it's my free time, I code, when it's not, I still code.
+          </h3>
           <br></br>
-          <br></br>Apart from coding, I shred my guitar during breaktimes.
-        </h4>
+          <br></br>
+          <h5>Apart from coding, I shred my guitar during breaktimes.</h5>
+        </TypewriterEffect>
       </div>
-
       <MyPic />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,31 @@
 import Head from "next/head";
-import Image from "next/image";
 import Navbar from "../components/Navbar";
 import MyPic from "../components/MyPic";
-// const mypic = require("../components/MyPic");
 import s from "../styles/Home.module.css";
-// import AnimatedText from "react-animated-text-content";
 import MyPicMini from "../components/MyPicMini";
 import { NAVBAR_ITEMS } from "../enums";
+import TypeWriterEffect from "../components/TypewriterEffect";
 
 export default function Home() {
-  // MyPic.map((i) => console.log(i));
-
   return (
     <div className={s.Home}>
       <Head>
         <title>Portfolio | Jeremiah Valencia</title>
-        <meta name="description" content="Home Jeremiah Valencia" />
+        <meta name="description" content="Portfolio Jeremiah Valencia" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Navbar active={NAVBAR_ITEMS.Home} />
       <MyPicMini />
       <div className={s.txt_container}>
-        <h5>&nbsp;&nbsp;Hello, I am</h5>
-        <h1>Jeremiah Valencia</h1>
-        <br></br>
-        <br></br>
-        <h3>Full Stack Developer</h3>
+        <TypeWriterEffect>
+          <h5 style={{ marginLeft: "16px" }}>Hello, I am</h5>
+          <h1>Jeremiah Valencia</h1>
+          <br></br>
+          <br></br>
+          <h3>Full Stack Developer</h3>
+        </TypeWriterEffect>
       </div>
-
       <MyPic />
-      {/* {mypic.default()} */}
     </div>
   );
 }
